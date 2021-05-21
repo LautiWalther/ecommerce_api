da4ba2bdce2c138d1dca040c3821dcb1be3d8ce8
Lauti01 <lautigeniop@gmail.com>
2021-05-20 21:01:50 -0300

    product_id added

--- src/routes/products.js
+++ src/routes/products.js
@@ -8,4 +8,10 @@ route.get('/', authToken, (req, res) => {
 	res.json(products);
 });
 
+route.get('/:product_id', authToken, (req, res) => {
+	if(isNaN(+req.params.product_id))return res.json({error:1, message:'product id param must be a number'});
+	var product_with_id = products.find(product => product.id === +req.params.product_id) || {};
+	res.json(product_with_id);
+});
+
 exports = module.exports = route;
